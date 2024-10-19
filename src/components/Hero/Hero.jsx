9481e6c950797b19741caee28858392a40c2b807
ShamsiddinWeb <shamsiddinnorov734@gmail.com>
2024-10-19 18:02:42 +0500

    telegramm

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.scss";
-import heroImg from "../../assets/images/heroImg.png"
+import heroImg from "../../assets/images/heroImg.png";
+import { BsTelegram } from "react-icons/bs";
 
 const Hero = (props) => {
   return (
@@ -12,6 +13,11 @@ const Hero = (props) => {
             <p className="hero__text1">{props.t("hero__text1")}</p>
             <p className="hero__text2">{props.t("hero__text2")}</p>
             <p className="hero__text3">{props.t("hero__text3")}</p>
+            <div className="telegram">
+              <a href="https://telegram.me/Bukharanaturalproduct">
+                <BsTelegram />
+              </a>
+            </div>
           </div>
         </div>
       </div>
